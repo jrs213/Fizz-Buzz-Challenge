c4e69b26a2e4dc7291fafcf55fa051fdacb43c3a
Jake Schmidt <jakeschmidt213@gmail.com>
2025-04-16 11:24:43 -0700

    Initial Fizz Buzz Logic

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-
 let numInput = document.getElementById("numInput");   
-    
 
-function log(){
+function log() {
     console.log(numInput.value);
+    let num = parseInt(numInput.value);
+// Create check to verify the input is a number and is positive 
+    console.log(num);
+
+    if ( num % 3 === 0 && num % 5 === 0){
+        console.log("The number is divisible by both 3 and 5");
+    } else if (num % 3 === 0){
+        console.log("The number is divisible by 3");
+    } else if (num % 5 === 0){
+        console.log("The number is divisible by 5");
+    } else {
+        console.log("The number is not divisible by 3 or 5");
+    }
 }
+
