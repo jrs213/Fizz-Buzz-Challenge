f6cab48cc8f2de558ca4ff70f3083dd1537b9739
Jake Schmidt <jakeschmidt213@gmail.com>
2025-04-16 13:47:07 -0700

    Updated logic

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
 let numInput = document.getElementById("numInput");   
 
 function FizzBuzz() {
-    let num = parseInt(numInput.value);
-    console.log(num);
-// Input Value Validation Check
-    if (isNaN(num)) {
-        console.log("Please enter a valid number");
-        return;
-    }
-    if (num < 0) {
-        console.log("Please enter a positive number");
-        return;
-    }
-    if (num === 0) {
-        console.log("The number is zero");
-        return;
-    }
-// FizzBuzz Logic
-    if (num % 3 === 0 && num % 5 === 0) {
-        console.log("FizzBuzz");
-    } else if (num % 3 === 0) {
-        console.log("Fizz");
-    } else if (num % 5 === 0) {
-        console.log("Buzz");
-    } else {
-        console.log(num);
-    }
-}
+    const results = [];
+    const map = { 3: "Fizz", 5: "Buzz" };
+    const divisors = [3,5];
 
+    for (let i = 1; i <= n; i++) {
+        let output = "";
+        for (let divisor of divisors) {
+            if (i % divisor === 0) {
+                output += map[divisor];
+            }
+        }
+        if (output === "") {
+            output += i;
+        }
+        results.push(output);
+    }
+    return results;
+}
+const n = 9; // You can change this value to test with different numbers
+const results = FizzBuzz(n);
+console.log(results.join(", "));
